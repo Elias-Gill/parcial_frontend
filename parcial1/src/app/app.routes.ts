99c0ff81081a6df0b81532a7c1b0f749f91dbf50
Elias Gill <eliasgill42@gmail.com>
2025-08-24 15:16:59 -0300

    fix: links del nav

--- parcial1/src/app/app.routes.ts
+++ parcial1/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Lazy loading de los módulos standalone o con routing module
+export const routes: Routes = [
+  {
+    path: 'turnos',
+    loadChildren: () => import('./turnos/turnos-routing-module').then((m) => m.TurnosRoutingModule),
+  },
+  {
+    path: 'recepcion',
+    loadChildren: () =>
+      import('./recepcion/recepcion-routing-module').then((m) => m.RecepcionRoutingModule),
+  },
+  {
+    path: 'proveedores',
+    loadChildren: () =>
+      import('./proveedores/proveedores-routing-module').then((m) => m.ProveedoresRoutingModule),
+  },
+  {
+    path: 'productos',
+    loadChildren: () =>
+      import('./productos/productos-routing-module').then((m) => m.ProductosRoutingModule),
+  },
+  {
+    path: 'jaulas',
+    loadChildren: () => import('./jaulas/jaulas-routing-module').then((m) => m.JaulasRoutingModule),
+  },
+  {
+    path: '',
+    redirectTo: 'turnos',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'turnos',
+  },
+];
